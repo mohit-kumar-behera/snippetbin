598d2b0063ca6be5e63d5d799f2f531362920464
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2022-02-16 15:04:30 +0530

    add snippet page

--- snippet/views.py
+++ snippet/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def snippet_view(request):
-  return HttpResponse('SNIPPET PAGE')
+  return render(request, 'snippet/snippet.html')
