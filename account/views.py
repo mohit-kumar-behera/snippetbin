967b32c24268d33a5a25865a5dbd1b2c1dd75ac7
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2022-02-16 12:01:51 +0530

    initial commit

---
+++ account/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def signup_view(request):
+  return HttpResponse('SIGNUP PAGE')
---
+++ snippet/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from snippet import views
+
+app_name = 'snippet'
+
+urlpatterns = [
+  path('', views.snippet_view, name = 'snippet'),
+]
